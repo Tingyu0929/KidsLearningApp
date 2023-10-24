97d38e056e3fab70511885559bf8dee7cc7049d6
Tingyu0929 <zzzz05719@gmail.com>
2023-10-24 14:13:03 +0800

    KidsLearningApp

---
+++ KidsLearningDetailPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsLearningApp;
+
+public partial class KidsLearningDetailPage : ContentPage
+{
+    public KidsLearningDetailPage(string categoryName)
+    {
+        InitializeComponent();
+        DisplayLearningResult(categoryName);
+        Title = categoryName;
+    }
+
+    private void DisplayLearningResult(string categoryName)
+    {
+        LearningData learningData = new LearningData();
+        switch (categoryName)
+        {
+            case "Numbers":
+                CvKids.ItemsSource = learningData.Numbers; break;
+            case "Letters":
+                CvKids.ItemsSource = learningData.Letters; break;
+            case "Shapes":
+                CvKids.ItemsSource = learningData.Shapes; break;
+            case "Colors":
+                CvKids.ItemsSource = learningData.Colors; break;
+        }
+    }
+
+    private async void CvKids_OnSelectionChanged_(object sender, SelectionChangedEventArgs e)
+    {
+        var selectedItem = e.CurrentSelection.FirstOrDefault() as LearningItem;
+        await TextToSpeech.SpeakAsync(selectedItem.Name);
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace KidsLearningApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        var commandParameter = (sender as ImageButton)?.CommandParameter as string;
+
+        // 在這裡處理按鈕點擊事件的邏輯
+        // 例如，你可以導航到相應的詳細頁面，傳遞類別名稱作為參數
+        if (!string.IsNullOrEmpty(commandParameter))
+        {
+            Navigation.PushAsync(new KidsLearningDetailPage(commandParameter));
+        }
+    }
+}
